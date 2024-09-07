7ad275646de499543c01d7b55c21befc9206e864
zehan12 <zehan9211@gmail.com>
2024-09-07 13:00:00 +0530

    add million js

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,10 @@
+import MillionLint from "@million/lint";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
         reactCompiler: true,
     },
 };
-
-export default nextConfig;
+export default MillionLint.next({
+    rsc: true,
+})(nextConfig);
